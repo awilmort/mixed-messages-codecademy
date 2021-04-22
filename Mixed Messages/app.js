70eae21b598eeadab7f97485cf80751b751d6424
Quentin Staley <qstaley921@gmail.com>
2021-04-22 11:47:38 -0400

    Adds Display Message to Inner HTML

--- Mixed Messages/app.js
+++ Mixed Messages/app.js
@@ -1,7 +1,37 @@
-import { generateMessage } from 'messageGenerator.js';
-//import { randomMessages } from './randomMessages.js';
+// import { generateMessage } from 'messageGenerator.js';
+// import { randomMessages } from './randomMessages.js';
+
+const btnElement = document.querySelector('button'); 
+const msgElement = document.querySelector('#message');
 
 const displayMessage = () => {
     const message = generateMessage(randomMessages);
-    console.log(message);
+    msgElement.innerHTML = message;
 };
+
+/** ******************************
+ * Messange Generator Below:
+ ******************************** */
+
+const randomMessages = {
+  oppening: ["Pair programming is", "Practice is"],
+  middle: ["key", "necessary", "the best idea"],
+  closing: ["to success", "to a better understanding"]
+};
+
+const chooseRandomMessage = (messages) => {
+  return messages[Math.floor(Math.random() * messages.length)];
+};
+
+const generateMessage = (randomMessages) => {
+  const oppening = chooseRandomMessage(randomMessages.oppening);
+  const middle = chooseRandomMessage(randomMessages.middle);
+  const closing = chooseRandomMessage(randomMessages.closing);
+  return oppening + ' ' + middle + ' ' + closing;
+}
+
+/**
+ * Event Listeners
+ */
+
+btnElement.addEventListener('click', displayMessage);
